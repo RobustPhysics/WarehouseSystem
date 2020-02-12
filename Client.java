b4f9c5d4f688f447806b1e58a47cc3aa7274f153
Miranda Lattrez <mirandayl1998@comcast.net>
2020-02-12 16:57:07 -0600

    stage 1 changes to client and client list

--- Client.java
+++ Client.java
@@ -1,5 +1,27 @@
 //Miranda
+//include constructor, get, set, variables as needed
+import java.util.*;
+import java.io.*;
 public class Client
 {
-	//test
+	private String id;
+	private String name;
+	
+	public Client (String id,String name){
+	this.id=id;
+	this.name=name;
+	}
+	
+	public String getId(){
+	return id;
+	}
+	
+	public String getName(){
+	return name;
+	}
+	
+	public String toString(){
+		String string=" id "+id + " name "+ name;
+		return string; 
+	}
 }
