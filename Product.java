3106cb7eb22a8342e0e2c87c05edc47ce6dd0226
Chris Leach <cdleach5@gmail.com>
2020-02-12 18:48:26 -0600

    Added to Product and ProductList

--- Product.java
+++ Product.java
@@ -1,5 +1,57 @@
 // Chris Leach
+
 public class Product
 {
-	
+    private int productID;          // Unique product ID
+    private String productName;     // Name of product
+    private double salePrice;       // Sale price of product
+    private int productQuantity;    // Avaliable quantity of product
+    
+    // Constructor
+    public Product() {
+        productID = 0;
+        productName = "";
+        salePrice = 0;
+        productQuantity = 0;
+    }
+
+    // Constructor
+    public Product(int id, String name, double price) {
+        productID = id;
+        productName = name;
+        salePrice = price;
+        productQuantity = 0;
+    }
+    
+    public int getProductID() {
+        return productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+
+    public int getQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductID(int id) {
+        productID = id;
+    }
+
+    public void setProductName(String name) {
+        productName = name;
+    }
+
+    public void setSalePrice(double price) {
+        salePrice = price;
+    }
+
+    public void setQuantity(int quantity) {
+        productQuantity = quantity;
+    }
 }
--- ProductList.java
+++ ProductList.java
@@ -1,11 +1,15 @@
+import java.util.List;
+
 // Chris Leach
+// Add, Serch
 public class ProductList
 {
 	private static ProductList singleton;
-	List<Product> products;
+	List<Product> products;                 // List of all products
 	
 	private ProductList()
 	{
+		products = null;                    // Initilizing an empty list for the products
 	}
 	
 	public static ProductList getInstance()
@@ -16,5 +20,33 @@ public class ProductList
 	public Iterator GetProducts()
 	{
 		return products.iterator();
+	}
+
+	public Product searchProduct(Product p) {
+		if (products.contains(p)) {
+
+		}
+		return p;
+	}
+	
+	// Returns true if the product is in the product list,
+	// returns false if it is not in the list
+	public boolean isInList(Product p) {
+		if (products.contains(p) == true)
+			return true;
+		else
+			return false;
+	}
+
+	// Adds one instance of a product into the system
+	public void addProduct(Product p) {
+		products.add(p);
+		p.setQuantity(p.getQuantity() + 1);   // Increasing product quantity by one
+	}
+
+	// Adds multiple instances of a product into the system
+	public void addProduct(Product p, int quantity) {
+		products.add(p);
+		p.setQuantity(p.getQuantity() + quantity); // Increase quantity
 	}
 }
