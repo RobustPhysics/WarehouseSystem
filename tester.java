a2dbdaebd159b5197dfb8111903f55783a931b5c
Carlson <cq4732ff@ahscentos.campus.stcloudstate.edu>
2020-02-13 15:59:11 -0600

    added some details to test

--- tester.java
+++ tester.java
@@ -10,19 +10,19 @@ class tester
 		
 		warehouse.addClient("237487","Bob Sagat");
 		warehouse.addClient("1234656","Hello Padre");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
-		warehouse.addClient("","");
+		warehouse.addClient("1234123","goodness gracious");
+		warehouse.addClient("12341234","hihowareyou");
+		warehouse.addClient("4234","whatsgoingon");
+		warehouse.addClient("856787","isaid");
+		warehouse.addClient("2341","hey");
+		warehouse.addClient("44324","hey");
+		warehouse.addClient("9484","hey");
+		warehouse.addClient("93402","hey");
+		warehouse.addClient("2394923","isaidhey");
+		warehouse.addClient("239023","whats going on");
+		warehouse.addClient("92034","clare");
+		warehouse.addClient("67565","chris");
+		warehouse.addClient("2342341","MIRANDANANA");
 
 		Iterator clients = warehouse.getClients();
 		while (clients.hasNext())
@@ -44,17 +44,21 @@ class tester
 		System.out.println("");
 		System.out.println();
 		
-		warehouse.addProduct();
-		warehouse.addProduct();
-		warehouse.addProduct();
-		warehouse.addProduct();
-		warehouse.addProduct();
-		warehouse.addProduct();
-		warehouse.addProduct();
-		warehouse.addProduct();
-		warehouse.addProduct();
-		warehouse.addProduct();
-		
+		warehouse.addProduct("982734","bay food",45.00,78);
+		warehouse.addProduct("98273","babyfood",45.00,78);
+		warehouse.addProduct("9827","baby ood",45.00,78);
+		warehouse.addProduct("9824","baby o",45.00,78);
+		warehouse.addProduct("98734","bab ood",45.00,78);
+		warehouse.addProduct("82734","bay food",45.00,78);
+		warehouse.addProduct("9834","bby food",45.00,78);
+		warehouse.addProduct("982734","bb food",45.00,78);
+		warehouse.addProduct("8274","ba food",45.00,78);
+		warehouse.addProduct("973","baod",45.00,78);
+		warehouse.addProduct("98273","bod",45.00,78);
+		warehouse.addProduct("94434","bab foo",45.00,78);
+		warehouse.addProduct("87","baby fod",45.00,78);
+		warehouse.addProduct("94","baby food",45.00,78);
+
 		Iterator products = warehouse.getProducts();
 		while (products.hasNext())
 		{
@@ -75,16 +79,18 @@ class tester
 		System.out.println("");
 		System.out.println();
 		
-		warehouse.addSupplier();
-		warehouse.addSupplier();
-		warehouse.addSupplier();
-		warehouse.addSupplier();
-		warehouse.addSupplier();
-		warehouse.addSupplier();
-		warehouse.addSupplier();
-		warehouse.addSupplier();
-		warehouse.addSupplier();
-		warehouse.addSupplier();
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
+		warehouse.addSupplier("MEDTRONIC","666 Satans Ave.");
 		
 		Iterator suppliers = warehouse.getSuppliers();
 		while (suppliers.hasNext())
