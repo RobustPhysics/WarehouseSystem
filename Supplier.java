3057cceb69d0fb1c237fad9ab20e37969b37e13b
ragtux <56554701+ragtux@users.noreply.github.com>
2020-02-12 19:16:33 -0600

    added basic methods based on Sarnath's Member class

--- Supplier.java
+++ Supplier.java
@@ -1,6 +1,81 @@
-//Barrett
+// Barrett
+// Supplier.java
 
-public class Supplier
-{
-	
+public class Supplier implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  private String supplierID;
+  private String name;
+  private String address;
+  private static final String SUPPLIER_STRING = "M";
+  private List<SuppliedProduct> suppliedproducts = new LinkedList<SuppliedProduct>();
+  
+  public Supplier (String name, String address)
+  {
+    // ID server??
+    //supplierID = SUPPLIER_STRING + ManufacturerIDServer.instance().getID();
+    this.name = name;
+    this.address = address;
+  }
+  
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  
+  public void setAddress(String address)
+  {
+    this.address = address;
+  }
+  
+  public String getSupplierID()
+  {
+    return supplierID;
+  }
+  
+  public String getName() 
+  {
+    return name;
+  }
+  
+  public String getAddress() {
+    return address;
+  }
+  
+  public Iterator getSuppliedProducts()
+  {
+    return suppliedProducts.iterator();
+  }
+
+  public boolean assignProduct(Supplies supplies)
+  {
+    return suppliedProducts.add(supplies);
+  }
+
+  public boolean unassignProduct(String productID, String manufacturerID)
+  {
+    Iterator suppliesIterator = suppliedProducts.iterator();
+
+    while (suppliesIterator.hasNext())
+    {
+      Supplies supplies = (Supplies)(suppliesIterator.next());
+      Manufacturer manufacturer = supplies.getManufacturer();
+      Product product = supplies.getProduct();
+      
+      if (product.getProductID().equals(productID) && manufacturer.getManufacturerID().equals(manufacturerID))
+      {
+        return suppliedProducts.remove(supplies);
+      }
+    }
+    
+    return false; //Couldn't find Supplies object to unassign
+   
+   }
+
+  
+  public String toString() {
+    String string = "Supplier ID: " + supplierID + " \nName: " + name + "\nAddress: " + address;
+    return string;
+  }
+
 }
--- SupplierList.java
+++ SupplierList.java
@@ -1,21 +1,74 @@
-//Barrett
+// Barrett Carlson
+// SupplierList.java
 
-public class SupplierList
-{
-	private static SupplierList singleton;
-	List<Supplier> suppliers;
-	
-	private SupplierList()
-	{
-	}
-	
-	public static SupplierList getInstance()
-	{
-		return singleton;
-	}
-	
-	public Iterator GetSuppliers()
-	{
-		return suppliers.iterator();
-	}
+public class SupplierList implements Serializable{
+
+  private static final long serialVersionUID = 1L;
+  private List<Supplier> suppliers = new LinkedList<Supplier>();
+  private static SupplierList supplierlist;
+
+  private MemberList() {}
+
+  public static SupplierList instance() {
+    if (supplierlist == null) {
+      return (supplierlist = new SupplierList());
+    } else {
+      return supplierlist;
+    }
+  }
+
+  public boolean insertSupplier(Supplier supplier) {
+    return suppliers.add(supplier);
+  }
+
+  public Supplier searchSuppler(String supplierID)
+  {
+    Iterator supplierIterator = suppliers.iterator();
+    
+    while (supplierIterator.hasNext())
+    {
+        Supplier supplier = (Supplier)(supplierIterator.next());
+        if (supplier.getSupplierID().equals(supplierID))
+        {
+            return manufacturer;
+        }
+    }
+    return null;
+  }
+  
+  public Iterator getSuppliers(){
+    return suppliers.iterator();
+  }
+  
+  private void writeObject(java.io.ObjectOutputStream output) {
+    try {
+      output.defaultWriteObject();
+      output.writeObject(supplierlist);
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
+    }
+  }
+  
+  private void readObject(java.io.ObjectInputStream input) {
+    try {
+      if (supplierlist != null) {
+        return;
+      } else {
+        input.defaultReadObject();
+        if (supplierlist == null) {
+          supplierlist = (SupplierList) input.readObject();
+        } else {
+          input.readObject();
+        }
+      }
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
+    } catch(ClassNotFoundException cnfe) {
+      cnfe.printStackTrace();
+    }
+  }
+  
+  public String toString() {
+    return suppliers.toString();
+  }
 }
