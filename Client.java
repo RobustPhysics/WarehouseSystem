8f4cb0c484b2a99a8a6d43c9c4a7fe10eea3efcc
ragtux <56554701+ragtux@users.noreply.github.com>
2020-02-20 12:00:45 -0600

    added ClientIdServer
    added file, and some minor changess

--- Client.java
+++ Client.java
@@ -1,28 +1,37 @@
 //Miranda
 //include constructor, get, set, variables as needed
+
 import java.util.*;
 import java.io.*;
+
 public class Client implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 	private String id;
 	private String name;
 	
-	public Client (String id,String name){
-	this.id=id;
-	this.name=name;
+	private static final String CLIENT_STRING = "C";
+	
+	public Client (String id, String name)
+	{
+		this.id=id;
+		this.name=name;
+		id = CLIENT_STRING + (ClientIdServer.instance()).getId();
 	}
 	
-	public String getId(){
-	return id;
+	public String getId()
+	{
+		return id;
 	}
 	
-	public String getName(){
-	return name;
+	public String getName()
+	{
+		return name;
 	}
 	
-	public String toString(){
-		String string=" id "+id + " name "+ name;
+	public String toString()
+	{
+		String string="id: "+id + "\nname: "+ name;
 		return string; 
 	}
 }
