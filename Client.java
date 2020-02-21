42a015f0bb873790ad5c5cd019b17eb5c6f5c124
Miranda Lattrez <mirandayl1998@comcast.net>
2020-02-21 16:28:32 -0600

    Update Client.java

--- Client.java
+++ Client.java
@@ -55,7 +55,7 @@ public class Client implements Serializable
 					String desc="Bought "+item.getProductQuantity()+ " of" +product.getName();
 					Invoice invoice=new Invoice(d.toString, product, desc, amountDue);
 					invoiceList.add(invoice);
-					client.incrementAmountDue(amountDue);
+					incrementAmountDue(amountDue);
 				}
 				else
 				{
