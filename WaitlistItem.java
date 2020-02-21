7281a84d52478ff3ac94155f7a4621f4af75ef5e
Chris Leach <cdleach5@gmail.com>
2020-02-21 15:27:21 -0600

    Update WaitlistItem.java

--- WaitlistItem.java
+++ WaitlistItem.java
@@ -1,14 +1,35 @@
-//???
+// Chris
 
 public class WaitlistItem {
 	//Stored in the product, contains information on
 	//client and quantity. Maybe price too? And date added to list?
+
+	private int quantity;
+	private Client client;
+	private double price;
+	private String date;      // Date added to the list
 	
+	public void setQuantity(int q) {
+		quantity = q;
+	}
+
+	public void setClient(Client c) {
+		client = c;
+	}
+
+	public void setPrice(double p) {
+		price = p;
+	}
+
+	public void setDate(String d) {
+		date = d;
+	}
 	
 	public WaitlistItem()
 	{
 		
 	}
+ 
 	
 	public String toString()
 	{
