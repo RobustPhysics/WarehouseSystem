1e429dd2ae960f97a9f107ed5e6fdb96368fae2e
ragtux <56554701+ragtux@users.noreply.github.com>
2020-02-21 15:33:46 -0600

    changes to waitlist
    some minor changes

--- WaitlistItem.java
+++ WaitlistItem.java
@@ -1,13 +1,24 @@
 // Chris
+import java.util.*;
+import java.io.*;
 
-public class WaitlistItem {
-	//Stored in the product, contains information on
-	//client and quantity. Maybe price too? And date added to list?
-
-	private int quantity;
+public class WaitlistItem implements Serializable{
+	// Stored in the product, contains information on
+	// client and quantity. Maybe price too? And date added to list?
+	private static final long serialVersionUID = 1L;
 	private Client client;
+	private Product product;
+	private int quantity;
 	private double price;
 	private String date;      // Date added to the list
+	
+	public WaitlistItem(Client client, Product product, int quantity)
+	{
+		this.client = client;
+		this.product = product; 
+		this.quantity = quantity;
+		
+	}
 	
 	public void setQuantity(int q) {
 		quantity = q;
@@ -25,14 +36,30 @@ public class WaitlistItem {
 		date = d;
 	}
 	
-	public WaitlistItem()
-	{
-		
+	public Client getClient(){
+		return client;
 	}
+	
+	public Product getProduct() {
+		return product;
+	}
+	
+	public int getQuantity(){
+		return quantity;
+	}
+	
+	public String getDate(){
+		return date;
+	}
+	
+	public void decrementQuantity(int quantity) {
+		this.quantity -= quantity;
+	}
+
  
 	
-	public String toString()
-	{
-		return "";
+	public String toString() {
+		return "Waitlist Item: \nClient ID: " + client.getId() + "\n" + product.toString() + "\nQuantity: " + quantity;
+		
 	}
 }
