b21e90b2c558fb08caaeecda783b95335c286c7f
Clare <robustphysics@gmail.com>
2020-02-21 15:23:03 -0600

    sdf
    sdf

---
+++ WaitlistItem.java
@@ -0,0 +1,17 @@
+//???
+
+public class WaitlistItem {
+	//Stored in the product, contains information on
+	//client and quantity. Maybe price too? And date added to list?
+	
+	
+	public WaitlistItem()
+	{
+		
+	}
+	
+	public String toString()
+	{
+		return "";
+	}
+}
