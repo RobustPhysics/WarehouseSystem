272409cbf8a41509bf7c604750c1d0290a257ca6
Clare <robustphysics@gmail.com>
2020-02-26 12:38:38 -0600

    Quick fix
    UserInterface.showOutstandingClients() does not need to check if client is null while iterator through list of clients

--- UserInterface.java
+++ UserInterface.java
@@ -387,15 +387,8 @@ public class UserInterface
 		while (outstandingClients.hasNext())
 		{
 			Client client = (Client) outstandingClients.next();
-			if (client != null)
-			{
-				System.out.println("\t" + client);
-				System.out.println("\t\tAmount due: " + client.getAmountDue());
-			}
-			else
-			{
-				//What to do if it's null??
-			}
+			System.out.println("\t" + client);
+			System.out.println("\t\tAmount due: " + client.getAmountDue());
 		}
 	}
 	
@@ -403,6 +396,7 @@ public class UserInterface
 	{
 		Iterator products = warehouse.getProducts();
 		int productIndex = 1;
+		
 		while (products.hasNext())
 		{
 			Product p = (Product) products.next();
