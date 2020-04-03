7b3e1bae2b782e256e56095a963eba633ec170a7
ragtux <56554701+ragtux@users.noreply.github.com>
2020-04-03 17:29:00 -0500

    changes

--- ManagerMenuState.java
+++ ManagerMenuState.java
@@ -10,7 +10,7 @@ public class ManagerMenuState extends WarehouseState
 	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	private static Warehouse warehouse;
 
-	private enum ManagerOption
+	private enum Option
 	{
 		//Add a product
 		ADD_PRODUCT("Adds product to system"),
@@ -34,9 +34,9 @@ public class ManagerMenuState extends WarehouseState
 		LOGOUT("Logs out of the Manager state"); //TODO
 
 		private String description;
-		private static int LENGTH = ManagerOption.values().length;
+		private static int LENGTH = Option.values().length;
 
-		private ManagerOption(String str)
+		private Option(String str)
 		{
 			description = str;
 		}
@@ -46,32 +46,47 @@ public class ManagerMenuState extends WarehouseState
 			return description;
 		}
 	}
-
+	
+	private ManagerMenuState()
+	{
+		warehouse = Warehouse.instance(); //get the facade
+		//context = WarehouseContext.instance();
+	}
+	
+	public static ManagerMenuState getInstance()
+	{
+		if (instance == null)
+		{
+			instance = new ManagerMenuState();
+		}
+		return instance;
+	}
+	
 	// displays menu
-	public void displayManagerHelp()
+	public void displayHelp()
 	{
 		System.out.println("Enter a number associated with a command seen below");
 		System.out.println("---------------------");
-		ManagerOption options[] = ManagerOption.values();
+		Option options[] = Option.values();
 
-		for (ManagerOption opt : options)
+		for (Option opt : options)
 		{
 			System.out.println(opt.ordinal() + " - " + opt.getDescription());
 		}
 	}
 
-	public ManagerOption getCommand()
+	public Option getCommand()
 	{
 		do
 		{
 			try
 			{
-				String token = getToken("Enter a command. Use " + ManagerOption.HELP.ordinal() + " to display the menu.");
+				String token = getToken("Enter a command. Use " + Option.HELP.ordinal() + " to display the menu.");
 				int value = Integer.parseInt(token);
-				if (value >= 0 && value <= ManagerOption.LENGTH)
+				if (value >= 0 && value <= Option.LENGTH)
 				{
 					System.out.println("IN THE getCommand() METHOD");
-					return ManagerOption.values()[value];
+					return Option.values()[value];
 				}
 				else
 				{
@@ -87,8 +102,8 @@ public class ManagerMenuState extends WarehouseState
 
 	public void process()
 	{
-		ManagerOption command;
-		displayManagerHelp();
+		Option command;
+		displayHelp();
 
 		do
 		{
